82c2965a4f7efc42a0290e324b9ac2afb8bb92a1
sanya2008 <74243016+sanya2008@users.noreply.github.com>
2021-01-16 15:10:04 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,16 @@
+class ground{
+    constructor(x,y,w,h){
+        var option={
+            isStatic:true
+        }
+        this.w=w
+        this.h=h
+        this.body=Bodies.rectangle(x,y,w,h,option)
+        World.add(world,this.body)
+    }
+    display(){
+        var pos=this.body.position
+        rectMode(CENTER)
+        rect(pos.x,pos.y,this.w,this.h)
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,64 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var lines1
+var lines2
+var lines3
+var paperobject
+var ground1
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(700, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+	lines1=new lines(240,600,20,140)
+	lines1.shapeColor="red"
+	lines2=new lines(350,650,200,20)
+	lines2.shapeColor="red"
+	lines3=new lines(450,600,20,140)
+	lines3.shapeColor="red"
+
+	paperobject=new paper(200,180,10)
+	paperobject.shapeColor="white"
+
+	ground1=new ground(350,350,30,700)
+	ground1.shapeColor="yellow"
+	
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("black");
+  lines1.display();
+  lines2.display();
+  lines3.display();
+  paperobject.display();
+  ground1.display();
+  drawSprites();
+ 
+}
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+Matter.Body.applyForce(paperobject.body,paperobject.body.position,{x:85,y:-85});
+	
+	 }
+   }
+
+
+
